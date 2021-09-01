9b7061a1db8e32423c68ecdc4781890bcb02faab
jim.wu <jimwu979@gmail.com>
2021-09-01 12:01:00 +0800

    [login] done

--- src/views/set/login.vue
+++ src/views/set/login.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="login">
-    
+    <div class="logo">
+      <img src="@/assets/logo.png">
+    </div>
+    <div class="input"
+         v-for="(item, index) in inputmodel" :key="index" 
+         :class="[{'focus': inputmodel[index].onFocus}, {'fill': inputmodel[index].val.length > 0}]">
+      <span>{{ inputmodel[index]['text'] }}</span>
+      <input @blur="focusInput(index, false)" 
+             @focus="focusInput(index, true)" 
+             v-model="inputmodel[index].val" 
+             :type="inputmodel[index]['type']">
+    </div>
+    <router-link class="loginbtn" to="index">登入</router-link>
+    <router-link class="signUp" to="signUp">註冊會員</router-link>
+    <div class="fixbox" :class="{'open': errorMessageIsOpen}">
+      <div class="lightbox">
+        email或密碼輸入錯誤
+        <div @click="closeLightbox">確定</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,8 +27,33 @@
 
 export default {
   name: 'login',
-  components: {
-      
-  }
+  data() {
+    return {
+      inputmodel: [
+        {
+          text: 'email',
+          type: 'account',
+          val: '',
+          onFocus: false
+        },
+        {
+          text: '密碼',
+          type: 'password',
+          val: '',
+          onFocus: false
+        }
+      ],
+      errorMessageIsOpen: false
+    }
+  },
+  methods: {
+    focusInput(targetInput, status){
+      this.inputmodel[targetInput].onFocus = status;
+    },
+    closeLightbox(){
+      this.errorMessageIsOpen = false;
+    },
+  },
 }
 </script>
+<style lang="scss" src="@/scss/set/login.scss"></style>

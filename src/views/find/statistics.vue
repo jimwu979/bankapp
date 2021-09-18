d3b4b2be2f9cc18497ea2c386d001b03f2cd6da2
jim.wu <jimwu979@gmail.com>
2021-09-18 15:05:59 +0800

    [statistics] done

--- src/views/find/statistics.vue
+++ src/views/find/statistics.vue
@@ -6,7 +6,7 @@
           <cssIcon_arrowLeft></cssIcon_arrowLeft>
         </div>
         <div class="center">
-          <monthlyCalendar></monthlyCalendar>
+          <monthlyCalendar :parentYear="year" :parentMonth="month"></monthlyCalendar>
         </div>
       </div>
     </header>
@@ -16,22 +16,22 @@
           <h2>{{ IncomeOrCost }}列表</h2>
           <div class="list">
             <router-link 
-              v-for="(item, index) in costList"
+              v-for="(item, index) in statisticsList"
               :key="index"
               v-clickStyle
               :to="{
                 path: 'classDetail', 
                 query: { 
-                  year: nowYear, 
-                  month: nowMonth, 
-                  isIncome: this.$route.query.isIncome, 
-                  class: item.class
+                  year: year, 
+                  month: month, 
+                  isIncome: isIncome, 
+                  classId: item.id,
                 }
               }">
               <div :class="['icon', 'color_' + item.color, 'icon_' + item.icon]"></div>
               <div class="information">
                 <div class="text">
-                  <div class="class">{{ item.class }}<span>{{ calculatePercentage[index] }}%</span></div>
+                  <div class="class">{{ item.className }}<span>{{ calculatePercentage[index] }}%</span></div>
                   <div class="number">{{ item.number }}</div>
                 </div>
                 <hr class="lineChart" :style="{width: calculatePercentage[index] + '%'}">
@@ -56,21 +56,59 @@ export default {
   },
   data() {
     return {
-      costList: [
-        { class: '正餐',    icon: 0, color: 0, number: 4820 },
-        { class: '生活費',  icon: 1, color: 1, number: 2749 },
-        { class: '其他',    icon: 2, color: 2, number: 2522 },
-        { class: '酒精',    icon: 3, color: 3, number: 1240 },
-        { class: '儲糧',    icon: 4, color: 4, number: 962 },
-        { class: '電話費',  icon: 5, color: 5, number: 609 },
-        { class: '飲料',    icon: 6, color: 6, number: 508 },
-        { class: '零時',    icon: 7, color: 7, number: 465 },
+      classList: [],
+      recordList: [],
+      statisticsList: [
+        // { id: 'zxc123', class: '正餐', icon: 0, color: 0, number: 2000 },
       ],
-      year: 2021,
-      month: 8,
-      // isIncome: JSON.parse(this.$route.query.isIncome),
+      year: 0,
+      month: 0,
       isIncome: false,
     }
+  },
+  created(){
+    let routeQuery = this.$route.query;
+    this.year     = routeQuery.year? Number(routeQuery.year) : new Date().getFullYear();
+    this.month    = routeQuery.month? Number(routeQuery.month) : new Date().getMonth() + 1;
+    this.isIncome = routeQuery.isIncome? routeQuery.isIncome : this.isIncome;
+    let xhr = new XMLHttpRequest();
+    let _this = this;
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState === 4 && xhr.status === 200){
+        let res = JSON.parse(xhr.response);
+        _this.classList = res.classList.filter(item => {
+          return item.typeIsIncome == _this.isIncome;
+        });
+        _this.recordList = res.record.filter(item => {
+          return item.typeIsIncome == _this.isIncome;
+        });
+        _this.classList.forEach(item => {
+          let sum = 0;
+          _this.recordList.forEach(recordItem => {
+            if(recordItem.classId == item._id) sum += recordItem.value;
+          })
+          _this.statisticsList.push({
+            classId: item._id,
+            className: item.className,
+            icon: item.iconImg,
+            color: item.iconColor,
+            number: sum,
+          });
+        });
+        _this.statisticsList.sort((a, b) => {
+          return b.number - a.number;
+        });
+      } 
+    };
+    xhr.open('post', '/api/readRecord_aMonth', false);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(JSON.stringify({
+      email: localStorage.getItem('email'),
+      loginCodeName: localStorage.getItem('loginCodeName'),
+      isIncome: this.isIncome,
+      year: this.year,
+      month: this.month,
+    }));
   },
   computed: {
     IncomeOrCost(){
@@ -78,14 +116,14 @@ export default {
     },
     totalCost(){
       let totalCost = 0;
-      this.costList.forEach(item => {
+      this.statisticsList.forEach(item => {
         totalCost += item.number;
       });
       return totalCost;
     },
     calculatePercentage(){
       let costPercentage = [];
-      this.costList.forEach(item => {
+      this.statisticsList.forEach(item => {
         let percentage = (item.number / this.totalCost) * 100;
         percentage = Math.round(percentage);
         costPercentage.push(percentage);

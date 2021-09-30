faab69691af2d1147e8a41b35c292e24244ad028
jim.wu <jimwu979@gmail.com>
2021-09-30 11:41:50 +0800

    💩[index] fix bug

--- src/views/find/index.vue
+++ src/views/find/index.vue
@@ -146,7 +146,7 @@ export default {
       let balance = income - cost;
       let selectMonth = this.$store.state.selectMonth;
       let daysLength = new Date(selectMonth.year, selectMonth.month, 0).getDate();
-      let remainDays = daysLength - new Date().getDate();
+      let remainDays = daysLength - new Date().getDate() + 1;
       let averageBalance = (balance / remainDays).toString();
       averageBalance = averageBalance.slice(0, averageBalance.indexOf('.'));
       
